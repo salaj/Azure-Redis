42f8398e43fd8506faf8870aa2d4238f387f3c01
Jakub Sala (JKSA) <jksa@demant-technology.com>
2018-12-21 00:25:55 +0100

    completed-2

--- ContosoTeamStats/Global.asax.cs
+++ ContosoTeamStats/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ContosoTeamStats.Models;
 
 namespace ContosoTeamStats
 {
@@ -12,6 +14,8 @@ namespace ContosoTeamStats
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //TODO:uncomment
+            Database.SetInitializer<TeamContext>(new TeamInitializer());
         }
     }
 }
