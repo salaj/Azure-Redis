8e221d7c88f3926f89ec398ef878d6e28c131185
Jakub Sala (JKSA) <jksa@demant-technology.com>
2018-12-20 23:34:28 +0100

    completed-1

--- ContosoTeamStats/Controllers/HomeController.cs
+++ ContosoTeamStats/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Configuration;
 using System.Web.Mvc;
+using StackExchange.Redis;
+
 //using StackExchange.Redis;
 
 namespace ContosoTeamStats.Controllers
@@ -27,44 +29,44 @@ namespace ContosoTeamStats.Controllers
         }
 
         //TODO:uncomment
-        //public ActionResult RedisCache()
-        //{
-        //    ViewBag.Message = "A simple example with Azure Cache for Redis on ASP.NET.";
+        public ActionResult RedisCache()
+        {
+            ViewBag.Message = "A simple example with Azure Cache for Redis on ASP.NET.";
 
-        //    var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-        //    {
-        //        string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
-        //        return ConnectionMultiplexer.Connect(cacheConnection);
-        //    });
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
 
-        //    // Connection refers to a property that returns a ConnectionMultiplexer
-        //    // as shown in the previous example.
-        //    IDatabase cache = lazyConnection.Value.GetDatabase();
+            // Connection refers to a property that returns a ConnectionMultiplexer
+            // as shown in the previous example.
+            IDatabase cache = lazyConnection.Value.GetDatabase();
 
-        //    // Perform cache operations using the cache object...
+            // Perform cache operations using the cache object...
 
-        //    // Simple PING command
-        //    ViewBag.command1 = "PING";
-        //    ViewBag.command1Result = cache.Execute(ViewBag.command1).ToString();
+            // Simple PING command
+            ViewBag.command1 = "PING";
+            ViewBag.command1Result = cache.Execute(ViewBag.command1).ToString();
 
-        //    // Simple get and put of integral data types into the cache
-        //    ViewBag.command2 = "GET Message";
-        //    ViewBag.command2Result = cache.StringGet("Message").ToString();
+            // Simple get and put of integral data types into the cache
+            ViewBag.command2 = "GET Message";
+            ViewBag.command2Result = cache.StringGet("Message").ToString();
 
-        //    ViewBag.command3 = "SET Message \"Hello! The cache is working from ASP.NET!\"";
-        //    ViewBag.command3Result = cache.StringSet("Message", "Hello! The cache is working from ASP.NET!").ToString();
+            ViewBag.command3 = "SET Message \"Hello! The cache is working from ASP.NET!\"";
+            ViewBag.command3Result = cache.StringSet("Message", "Hello! The cache is working from ASP.NET!").ToString();
 
-        //    // Demonstrate "SET Message" executed as expected...
-        //    ViewBag.command4 = "GET Message";
-        //    ViewBag.command4Result = cache.StringGet("Message").ToString();
+            // Demonstrate "SET Message" executed as expected...
+            ViewBag.command4 = "GET Message";
+            ViewBag.command4Result = cache.StringGet("Message").ToString();
 
-        //    // Get the client list, useful to see if connection list is growing...
-        //    ViewBag.command5 = "CLIENT LIST";
-        //    ViewBag.command5Result = cache.Execute("CLIENT", "LIST").ToString().Replace(" id=", "\rid=");
+            // Get the client list, useful to see if connection list is growing...
+            ViewBag.command5 = "CLIENT LIST";
+            ViewBag.command5Result = cache.Execute("CLIENT", "LIST").ToString().Replace(" id=", "\rid=");
 
-        //    lazyConnection.Value.Dispose();
+            lazyConnection.Value.Dispose();
 
-        //    return View();
-        //}
+            return View();
+        }
     }
 }
